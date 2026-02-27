41973469f7046dbdf6143f101a3cdf4f7791f526
shauryacollaboration-maker <shauryacollaboration@gmail.com>
2026-02-27 12:46:37 +0530

    Add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Instagram"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          "Instagram Clone UI",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
